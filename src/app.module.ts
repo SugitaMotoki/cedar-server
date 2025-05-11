a88d5587db999cd135987af8420d59fa1003c37c
SugitaMotoki <s.motoki21@gmail.com>
2025-05-11 22:59:58 +0900

    簡易環境変数読み取り機能を追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,16 +3,18 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: "postgres",
       host: "localhost",
       port: 5432,
-      username: "xx",
-      password: "xx",
-      database: "xx",
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       autoLoadEntities: true,
       synchronize: true,
     }),
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
   getHello(): string {
+    console.log({
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    });
     return "Hello World!";
   }
 }
